012ce0575147282949b01e183b89a8babab0af1d
Ian Carlson <carlsoni@sonoma.edu>
2025-04-11 23:35:00 -0700

    #1: don't require cam on hard coded channel ids.

--- src/services/bot-dispatcher.ts
+++ src/services/bot-dispatcher.ts
@@ -35,6 +35,12 @@ export const handleEvent = async (
     case "cameraOn":
       await serverUnmuteMember(guild, member)
       break
+    case "screenUnshared":
+      await serverUnmuteMember(guild, member)
+      break
+    case "screenShared":
+      await checkScreenShareAllowed(guild, member)
+      break
   }
 }
 
@@ -75,10 +81,21 @@ export const serverMuteMember = async (guild: Guild, member: GuildMember) => {
     return
   }
 
-  await member.edit({ mute: true }).catch((e) => {
-    lerror(e)
-  })
-  log(`${guild.name}: Muted ${member.user.username}`)
+  const noCameraRequireChannelIds = [
+      // oldrubberboot guild
+      '1360502181404344352', // No Camera Required
+      // Tavern guild
+      '779331254705061908',
+      '1360463558487314552',
+  ]
+  if (noCameraRequireChannelIds.includes(member.voice.channel.id)) {
+    log(`${guild.name}: No camera required channel:${member.voice.channel?.name}`)
+  } else {
+    await member.edit({ mute: true }).catch((e) => {
+      lerror(e)
+    })
+    log(`${guild.name}: Muted ${member.user.username}`)
+  }
 }
 
 export const serverUnmuteMember = async (guild: Guild, member: GuildMember) => {
@@ -96,4 +113,18 @@ export const serverUnmuteMember = async (guild: Guild, member: GuildMember) => {
     // log("Second try for good measure")
   }, 1500)
   log(`${guild.name}: Unmuted ${member.user.username}`)
+}
+
+
+export const checkScreenShareAllowed = async (
+    guild: Guild,
+    member: GuildMember
+) => {
+  //check if a bronze channel
+  // if bronze channel start 1 min timer that kicks the user
+  // mute the user
+  // message user
+
+  // if stop screen share unmute the user
+  // if stop screen share and not in a bronze channel unmute the user
 }
